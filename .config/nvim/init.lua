76e6e07d1edc3a6648e6c28129515de58880a255
Jakub Kaczmarek <kuba300698@gmail.com>
2022-06-10 20:31:21 +0200

    Add lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,9 @@
-require('globals')
 require('keymaps')
+require('globals')
 require('packer_init')
 
 require('plugins/lspconfig')
 require('plugins/cmp')
 require('plugins/treesitter')
 require('plugins/telescope')
+require('plugins/lualine')
