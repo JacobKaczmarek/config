1a118ab8695ef70b8e6d3d84e51ae29f88b2fabb
Jakub Kaczmarek <kuba300698@gmail.com>
2022-06-10 18:14:26 +0200

    Add alpha-vim

--- .config/nvim/lua/packer_init.lua
+++ .config/nvim/lua/packer_init.lua
@@ -1,6 +1,14 @@
 return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    
+    use {
+            "goolord/alpha-nvim",
+            config = function()
+                require("plugins/alpha") -- include you config
+            end,
+        }
+    
     use 'christoomey/vim-tmux-navigator'
 
     use 'neovim/nvim-lspconfig'
@@ -12,8 +20,10 @@ return require('packer').startup(function()
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- Telescope
+    use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'} }}
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use { "nvim-telescope/telescope-file-browser.nvim" }
 end)
