869136021486d446a6cd076caff970d591789a10
Jakub Kaczmarek <kuba300698@gmail.com>
2022-07-04 12:22:30 +0200

    Add colorschemes, null-ls and trouble plugins

--- .config/nvim/lua/packer_init.lua
+++ .config/nvim/lua/packer_init.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function()
                 require("plugins/alpha") -- include you config
             end,
     }
-    
+
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
@@ -21,7 +21,7 @@ return require('packer').startup(function()
         },
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
-   
+
     use 'christoomey/vim-tmux-navigator'
 
     use 'williamboman/nvim-lsp-installer'
@@ -30,6 +30,7 @@ return require('packer').startup(function()
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
+    use 'jose-elias-alvarez/null-ls.nvim'
 
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
@@ -37,8 +38,13 @@ return require('packer').startup(function()
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'kdheepak/lazygit.nvim'
 
+    use 'folke/trouble.nvim'
     -- Telescope
     use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'} }}
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use { "nvim-telescope/telescope-file-browser.nvim" }
+
+    -- Colorschemes
+    use 'sainnhe/edge'
+    use 'christianchiarulli/nvcode-color-schemes.vim'
 end)
